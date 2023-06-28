6deaf9b0d75aebbc1b18248496a6067e9c40fe97
Michael Ojo <trillzglobal@gmail.com>
2023-06-28 11:59:23 +0100

    positive integer

--- utils/amount.ts
+++ utils/amount.ts
@@ -1,3 +1,9 @@
 export const amountToKobo = (amount: number) =>{
     return amount * 100;
 }
+
+export const isPositiveInteger = (value)=> {
+    const parsedValue = parseInt(value);
+    return Number.isInteger(parsedValue) && parsedValue > 0;
+}
+  

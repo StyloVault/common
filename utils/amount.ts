f94b37f9f949a4f553ae9dedd93951dfcc669ca3
Michael Ojo <trillzglobal@gmail.com>
2023-06-28 12:09:50 +0100

    valid amount

--- utils/amount.ts
+++ utils/amount.ts
@@ -7,3 +7,8 @@ export const isPositiveInteger = (value)=> {
     return Number.isInteger(parsedValue) && parsedValue > 0;
 }
   
+export const isValidAmount = (amount) => {
+    // Regular expression pattern to match amount with maximum 2 decimal places
+    const pattern = /^\d+(\.\d{1,2})?$/;
+    return pattern.test(amount);
+  }

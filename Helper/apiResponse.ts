063bea9dcd76fe6548f6e2c77efad103a150570f
Hussayn_dev/ <hussaynabdsamad07@gmail.com>
2023-10-22 13:16:11 +0100

    Response format

--- Helper/apiResponse.ts
+++ Helper/apiResponse.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ApiResponseEnum } from './../enums/ApiResponse';
 @Injectable()
 export class ApiResponse {
-   success(message: string = '', data: any[] = [], code: number = 200): any {
+   success(message: string = '', data: any = [], code: number = 200): any {
     const response = {
       code: code,
       status: ApiResponseEnum.StatusSuccess,
@@ -15,7 +15,7 @@ export class ApiResponse {
     return response;
   }
 
-   failure(message: string = '', data: any[] = [], code: number = 400): any {
+   failure(message: string = '', data: any = [], code: number = 400): any {
     const response = {
       code: code,
       status: ApiResponseEnum.StatusFailed,

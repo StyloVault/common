e5390796797ad2e07f3e31cc39dc4cf540aa82e3
Hussayn_dev/ <hussaynabdsamad07@gmail.com>
2023-10-22 13:01:14 +0100

    Response format

---
+++ Helper/apiResponse.ts
@@ -0,0 +1,42 @@
+import { ApiResponseEnum } from './../enums/ApiResponse'; // Import your ApiResponseEnum
+
+export class ApiResponse {
+   success(message: string = '', data: any[] = [], code: number = 200): any {
+    const response = {
+      code: code,
+      status: ApiResponseEnum.StatusSuccess,
+      message: message,
+      result: {
+        data: data,
+      },
+    };
+
+    return response;
+  }
+
+   failure(message: string = '', data: any[] = [], code: number = 400): any {
+    const response = {
+      code: code,
+      status: ApiResponseEnum.StatusFailed,
+      message: message,
+      result: {
+        data: data,
+      },
+    };
+
+    return response;
+  }
+
+   exceptionResponse(exception: any): any {
+    console.log(exception.message);
+
+    return {
+      status: ApiResponseEnum.StatusFailed,
+      message: 'An unexpected error was encountered. Please contact the Admin.',
+      result: {
+        data: null,
+      },
+      error: exception.message,
+    };
+  }
+}
---
+++ enums/ApiResponse.ts
@@ -0,0 +1,4 @@
+export enum ApiResponseEnum {
+    StatusSuccess='sucess',
+    StatusFailed='failed',
+}

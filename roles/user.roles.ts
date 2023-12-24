dfa3b93d74ae008aa5b1c8fd5292409942dbdd3a
Hussayn_dev/ <hussaynabdsamad07@gmail.com>
2023-12-25 00:19:50 +0100

    another push

--- roles/user.roles.ts
+++ roles/user.roles.ts
@@ -1,6 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Roles } from 'decorator/roles';
+import { Roles } from './../decorator/roles';
 
 @Injectable()
 export class UserRolesGuard implements CanActivate {
